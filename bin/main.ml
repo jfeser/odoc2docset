798693a5f126e30826d7c824d5d9bf918a9cc1bd
Jack Feser <feser@csail.mit.edu>
2017-12-24 14:17:18 -0800

    Major performance improvements.

--- bin/main.ml
+++ bin/main.ml
@@ -3,15 +3,6 @@ open Printf
 open Bos
 open DocOck
 
-let read_file cmi =
-  match read_cmi (fun name _ -> name) cmi with
-  | Not_an_interface -> failwith "not an interface"
-  | Wrong_version -> failwith "wrong OCaml version"
-  | Corrupted -> failwith "corrupted"
-  | Not_a_typedtree -> failwith "not a typedtree"
-  | Not_an_implementation -> failwith "not an implementation"
-  | Ok intf -> intf
-
 let insert db name typ path =
   let query = sprintf "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('%s', '%s', '%s');" name typ path
   in
@@ -45,50 +36,110 @@ let plist pkg_name =
     |}
     pkg_name pkg_name pkg_name
 
-let process_pkg db name unit docu_dir =
-  let any = Paths.Identifier.any in
+(** Collect the identifiers (labeled with Dash types) in a compilation unit. *)
+let ids_of_unit : 'a Types.Unit.t -> ('a Paths.Identifier.any * string) list =
+  fun unit ->
+    let output = ref [] in
+    let index id type_ = output := (Paths.Identifier.any id, type_) :: !output in
 
-  let url id =
-    let open DocOckHtml in
-    match Url.from_identifier ~get_package:(fun _ -> name) ~stop_before:false id with
-    | Ok { page; anchor; kind } ->
-      List.rev ("index.html" :: page)
-      |> String.concat "/"
-      |> (fun path -> if anchor = "" then path else path ^ "#" ^ anchor)
-    | Error e -> failwith (Url.Error.to_string e)
-  in
-
-  let add_anchor id type_ name_ =
-    let anchor_elem =
-      Soup.create_element
-        ~attributes:["name", sprintf "//apple_ref/cpp/%s/%s" type_ name_]
-        ~class_:"dashAnchor"
-        "a"
+    let open Types in
+    let nop = fun _ -> () in
+    let rec process_module_decl =
+      let open ModuleType in
+      let rec process_module_type_expr = function
+        | Signature s -> process_signature s
+        | Functor (_, x) -> process_module_type_expr x
+        | _ -> ()
+      in
+      let open Module in
+      function
+      | Alias _ -> ()
+      | ModuleType e -> process_module_type_expr e
+    and process_module Module.({ id; type_; expansion; hidden }) =
+      let open Module in
+      if not hidden then begin
+        index id "Module";
+        process_module_decl type_;
+        begin match expansion with
+          | Some (Signature s)
+          | Some (Functor (_, s)) -> process_signature s
+          | _ -> ()
+        end
+      end
+    and process_module_type ModuleType.({ id }) = index id "Interface"
+    and process_type_ext = nop
+    and process_type TypeDecl.({ id }) = index id "Type"
+    and process_exception Exception.({ id }) = index id "Exception"
+    and process_val id type_ =
+      let open TypeExpr in
+      let kind = match type_ with
+        | Arrow _ -> "Function"
+        | Object _ -> "Object"
+        | _ -> "Value"
+      in
+      index id kind
+    and process_value Value.({ id; type_ }) = process_val id type_
+    and process_external External.({ id; type_ }) = process_val id type_
+    and process_method Method.({ id; private_ }) =
+      if not private_ then index id "Method"
+    and process_instance_variable InstanceVariable.({ id }) = index id "Variable"
+    and process_class_signature ClassSignature.({ items }) =
+      let open ClassSignature in
+      List.iter (function
+          | Method x -> process_method x
+          | InstanceVariable x -> process_instance_variable x
+          | _ -> ()) items
+    and process_class Class.({ id; type_ }) =
+      let open Class in
+      let rec process_decl = function
+        | ClassType (Signature x) -> process_class_signature x
+        | ClassType (Constr _) -> ()
+        | Arrow (_, _, d) -> process_decl d
+      in
+      process_decl type_;
+      index id "Class"
+    and process_class_type = nop
+    and process_include Include.({ expansion = { content } }) =
+      process_signature content
+    and process_signature_item =
+      let open Signature in
+      function
+      | Module x -> process_module x
+      | ModuleType x -> process_module_type x
+      | Type x -> process_type x
+      | TypExt x -> process_type_ext x
+      | Exception x -> process_exception x
+      | Value x -> process_value x
+      | External x -> process_external x
+      | Class x -> process_class x
+      | ClassType x -> process_class_type x
+      | Include x -> process_include x
+      | Comment x -> ()
+    and process_signature sig_ = List.iter process_signature_item sig_
+    and process_unit Unit.({ id; content }) =
+      let process_packed items =
+        List.iter (fun Unit.Packed.({ id; path }) -> index id "Module") items
+      in
+      match content with
+      | Module x -> index id "Module"; process_signature x
+      | Pack x -> process_packed x
     in
-    let open DocOckHtml in
-    let file, anchor =
-      match Url.from_identifier ~get_package:(fun _ -> name) ~stop_before:false id with
-      | Ok { page; anchor; kind } ->
-        let file =
-          let html_path =
-            List.rev ("index.html" :: page)
-            |> String.concat "/"
-            |> Fpath.of_string |> ok_exn
-          in
-          Fpath.(docu_dir // html_path)
-        in
-        (file, anchor)
-      | Error e -> failwith (Url.Error.to_string e)
-    in
-    if anchor = "" || not (OS.File.exists file |> ok_exn) then () else
-      let soup = OS.File.read file |> ok_exn |> Soup.parse in
-      begin match Soup.select_one (sprintf {|a[href="#%s"]|} anchor) soup with
-        | Some node -> Soup.prepend_child node anchor_elem
-        | None -> eprintf "Warning: Could not find anchor node for %s in %s.\n" anchor (Fpath.to_string file)
-      end;
-      OS.File.write file (Soup.to_string soup) |> ok_exn
-  in
+    process_unit unit; !output
 
+let update_index :
+  pkg:string -> Sqlite3.db -> ('a Paths.Identifier.any * string) list -> unit =
+  let ok_exn =
+    let open Sqlite3.Rc in
+    function
+    | OK -> ()
+    | e -> failwith (sprintf "Sqlite error: %s" (to_string e))
+  in
+  let done_exn =
+    let open Sqlite3.Rc in
+    function
+    | DONE -> ()
+    | e -> failwith (sprintf "Sqlite error: %s" (to_string e))
+  in
   let module Id = Paths.Identifier in
   let rec name : 'a Id.any -> string =
     function
@@ -111,95 +162,72 @@ let process_pkg db name unit docu_dir =
     | Id.Argument _
     | Id.Field _ -> failwith "No printable path."
   in
+  fun ~pkg db ids ->
+    let url id =
+      let open DocOckHtml in
+      match Url.from_identifier ~get_package:(fun _ -> pkg) ~stop_before:false id with
+      | Ok { page; anchor; kind } ->
+        List.rev ("index.html" :: page)
+        |> String.concat "/"
+        |> (fun path -> if anchor = "" then path else path ^ "#" ^ anchor)
+      | Error e -> failwith (Url.Error.to_string e)
+    in
 
-  let index id type_ =
-    insert db (name (any id)) type_ (url id);
-    add_anchor id type_ (Paths.Identifier.name id)
-  in
+    let open Sqlite3 in
+    let stmt = prepare db
+        "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?);"
+    in
+    exec db "BEGIN TRANSACTION;" |> ok_exn;
+    List.iter (fun (id, type_) ->
+        let open Data in
+        bind stmt 1 (TEXT (name id)) |> ok_exn;
+        bind stmt 2 (TEXT type_) |> ok_exn;
+        bind stmt 3 (TEXT (url id)) |> ok_exn;
+        step stmt |> done_exn;
+        reset stmt |> ok_exn;
+      ) ids;
+    exec db "END TRANSACTION;" |> ok_exn
 
-  let open Types in
-  let nop = fun _ -> () in
-  let rec process_module_decl =
-    let open ModuleType in
-    let rec process_module_type_expr = function
-      | Signature s -> process_signature s
-      | Functor (_, x) -> process_module_type_expr x
-      | _ -> ()
-    in
-    let open Module in
-    function
-    | Alias _ -> ()
-    | ModuleType e -> process_module_type_expr e
-  and process_module Module.({ id; type_; expansion; hidden }) =
-    let open Module in
-    if not hidden then begin
-      index id "Module";
-      process_module_decl type_;
-      begin match expansion with
-        | Some (Signature s)
-        | Some (Functor (_, s)) -> process_signature s
-        | _ -> ()
-      end
-    end
-  and process_module_type ModuleType.({ id }) = index id "Interface"
-  and process_type_ext = nop
-  and process_type TypeDecl.({ id }) = index id "Type"
-  and process_exception Exception.({ id }) = index id "Exception"
-  and process_val id type_ =
-    let open TypeExpr in
-    let kind = match type_ with
-      | Arrow _ -> "Function"
-      | Object _ -> "Object"
-      | _ -> "Value"
-    in
-    index id kind
-  and process_value Value.({ id; type_ }) = process_val id type_
-  and process_external External.({ id; type_ }) = process_val id type_
-  and process_method Method.({ id; private_ }) =
-    if not private_ then index id "Method"
-  and process_instance_variable InstanceVariable.({ id }) = index id "Variable"
-  and process_class_signature ClassSignature.({ items }) =
-    let open ClassSignature in
-    List.iter (function
-        | Method x -> process_method x
-        | InstanceVariable x -> process_instance_variable x
-        | _ -> ()) items
-  and process_class Class.({ id; type_ }) =
-    let open Class in
-    let rec process_decl = function
-      | ClassType (Signature x) -> process_class_signature x
-      | ClassType (Constr _) -> ()
-      | Arrow (_, _, d) -> process_decl d
-    in
-    process_decl type_;
-    index id "Class"
-  and process_class_type = nop
-  and process_include Include.({ expansion = { content } }) =
-    process_signature content
-  and process_signature_item =
-    let open Signature in
-    function
-      | Module x -> process_module x
-      | ModuleType x -> process_module_type x
-      | Type x -> process_type x
-      | TypExt x -> process_type_ext x
-      | Exception x -> process_exception x
-      | Value x -> process_value x
-      | External x -> process_external x
-      | Class x -> process_class x
-      | ClassType x -> process_class_type x
-      | Include x -> process_include x
-      | Comment x -> ()
-  and process_signature sig_ = List.iter process_signature_item sig_
-  and process_unit Unit.({ id; content }) =
-    let process_packed items =
-      List.iter (fun Unit.Packed.({ id; path }) -> index id "Module") items
-    in
-    match content with
-    | Module x -> index id "Module"; process_signature x
-    | Pack x -> process_packed x
-  in
-  process_unit unit
+let add_anchors : pkg:string -> Fpath.t -> ('a Paths.Identifier.any * string) list -> unit =
+  fun ~pkg docu_dir ids ->
+    List.fold_left (fun m (id, type_) ->
+        let open DocOckHtml in
+        let url =
+          Url.from_identifier ~get_package:(fun _ -> pkg) ~stop_before:false id
+        in
+        let Url.({ page; anchor }) = match url with
+          | Ok x -> x
+          | Error e -> failwith (Url.Error.to_string e)
+        in
+        let file =
+          let html_path =
+            List.rev ("index.html" :: page)
+            |> String.concat "/"
+            |> Fpath.of_string |> ok_exn
+          in
+          Fpath.(docu_dir // html_path)
+        in
+        if anchor = "" || not (OS.File.exists file |> ok_exn) then m else
+          let name_ = Paths.Identifier.name id in
+          let anchor_elem =
+            Soup.create_element
+              ~attributes:["name", sprintf "//apple_ref/cpp/%s/%s" type_ name_]
+              ~class_:"dashAnchor"
+              "a"
+          in
+          Fpath.Map.update file (function
+              | Some xs -> Some ((anchor, anchor_elem)::xs)
+              | None -> Some [anchor, anchor_elem]) m
+      ) Fpath.Map.empty ids
+    |> Fpath.Map.iter (fun file anchors ->
+        let soup = OS.File.read file |> ok_exn |> Soup.parse in
+        List.iter (fun (anchor, anchor_elem) -> 
+            match Soup.select_one (sprintf {|a[href="#%s"]|} anchor) soup with
+              | Some node -> Soup.prepend_child node anchor_elem
+              | None -> eprintf "Warning: Could not find anchor node for %s in %s.\n"
+                          anchor (Fpath.to_string file))
+          anchors;
+        OS.File.write file (Soup.to_string soup) |> ok_exn)
 
 let main output_path pkg_names =
   (* Create the docset template. *)
@@ -265,7 +293,10 @@ let main output_path pkg_names =
             let env = Odoc.Env.build builder (`Unit unit) in
             let expander = Odoc.Env.expander env in
             let unit = try DocOck.expand expander unit with _ -> unit in
-            process_pkg db name unit docu_dir));
+            let ids = ids_of_unit unit in
+            update_index ~pkg:name db ids;
+            add_anchors ~pkg:name docu_dir ids
+        ));
   eprintf " done.\n"
 
 let () =
