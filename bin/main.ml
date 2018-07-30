a58e8e44e0e8fae7f9afb4fd679f47bca9e9979f
Jack Feser <feser@csail.mit.edu>
2018-07-30 17:56:41 -0400

    Keep up with changes in odoc.

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
+open Base
 open Printf
 open Bos
-open DocOck
 
 let insert db name typ path =
   let query =
@@ -12,9 +12,11 @@ let insert db name typ path =
   (* printf "%s\n" query; *)
   ignore (Sqlite3.exec db query)
 
-let ok_exn : ('a, [`Msg of string]) Pervasives.result -> 'a = function
+let ok_exn : ('a, [`Msg of string]) Caml.Pervasives.result -> 'a = function
   | Ok x -> x
-  | Error (`Msg e) -> failwith e
+  | Error (`Msg e) ->
+      Logs.err (fun m -> m "%s" e) ;
+      Caml.exit 1
 
 let plist pkg_name =
   sprintf
@@ -40,11 +42,12 @@ let plist pkg_name =
     pkg_name pkg_name pkg_name
 
 (** Collect the identifiers (labeled with Dash types) in a compilation unit. *)
-let ids_of_unit : 'a Types.Unit.t -> ('a Paths.Identifier.any * string) list =
- fun unit ->
+let ids_of_unit unit =
+  let open Model.Lang in
   let output = ref [] in
-  let index id type_ = output := (Paths.Identifier.any id, type_) :: !output in
-  let open Types in
+  let index id type_ =
+    output := (Model.Paths.Identifier.any id, type_) :: !output
+  in
   let nop _ = () in
   let rec process_module_decl =
     let open ModuleType in
@@ -55,18 +58,19 @@ let ids_of_unit : 'a Types.Unit.t -> ('a Paths.Identifier.any * string) list =
     in
     let open Module in
     function Alias _ -> () | ModuleType e -> process_module_type_expr e
-  and process_module Module.({id; type_; expansion; hidden}) =
+  and process_module Module.({id; type_; expansion; hidden; display_type; _}) =
     let open Module in
     if not hidden then (
       index id "Module" ;
-      process_module_decl type_ ;
+      let module_type = Option.value ~default:type_ display_type in
+      process_module_decl module_type ;
       match expansion with
       | Some (Signature s) | Some (Functor (_, s)) -> process_signature s
-      | _ -> () )
-  and process_module_type ModuleType.({id}) = index id "Interface"
+      | None -> () )
+  and process_module_type ModuleType.({id; _}) = index id "Interface"
   and process_type_ext = nop
-  and process_type TypeDecl.({id}) = index id "Type"
-  and process_exception Exception.({id}) = index id "Exception"
+  and process_type TypeDecl.({id; _}) = index id "Type"
+  and process_exception Exception.({id; _}) = index id "Exception"
   and process_val id type_ =
     let open TypeExpr in
     let kind =
@@ -76,20 +80,21 @@ let ids_of_unit : 'a Types.Unit.t -> ('a Paths.Identifier.any * string) list =
       | _ -> "Value"
     in
     index id kind
-  and process_value Value.({id; type_}) = process_val id type_
-  and process_external External.({id; type_}) = process_val id type_
-  and process_method Method.({id; private_}) =
+  and process_value Value.({id; type_; _}) = process_val id type_
+  and process_external External.({id; type_; _}) = process_val id type_
+  and process_method Method.({id; private_; _}) =
     if not private_ then index id "Method"
-  and process_instance_variable InstanceVariable.({id}) = index id "Variable"
-  and process_class_signature ClassSignature.({items}) =
+  and process_instance_variable InstanceVariable.({id; _}) =
+    index id "Variable"
+  and process_class_signature ClassSignature.({items; _}) =
     let open ClassSignature in
     List.iter
-      (function
+      ~f:(function
         | Method x -> process_method x
         | InstanceVariable x -> process_instance_variable x
         | _ -> ())
       items
-  and process_class Class.({id; type_}) =
+  and process_class Class.({id; type_; _}) =
     let open Class in
     let rec process_decl = function
       | ClassType (Signature x) -> process_class_signature x
@@ -98,7 +103,7 @@ let ids_of_unit : 'a Types.Unit.t -> ('a Paths.Identifier.any * string) list =
     in
     process_decl type_ ; index id "Class"
   and process_class_type = nop
-  and process_include Include.({expansion= {content}}) =
+  and process_include Include.({expansion= {content; _}; _}) =
     process_signature content
   and process_signature_item =
     let open Signature in
@@ -113,11 +118,13 @@ let ids_of_unit : 'a Types.Unit.t -> ('a Paths.Identifier.any * string) list =
     | Class x -> process_class x
     | ClassType x -> process_class_type x
     | Include x -> process_include x
-    | Comment x -> ()
-  and process_signature sig_ = List.iter process_signature_item sig_
-  and process_unit Unit.({id; content; hidden}) =
+    | Comment _ -> ()
+  and process_signature sig_ = List.iter ~f:process_signature_item sig_
+  and process_unit Compilation_unit.({id; content; hidden; _}) =
     let process_packed items =
-      List.iter (fun Unit.Packed.({id; path}) -> index id "Module") items
+      List.iter
+        ~f:(fun Compilation_unit.Packed.({id; _}) -> index id "Module")
+        items
     in
     if not hidden then
       match content with
@@ -126,8 +133,7 @@ let ids_of_unit : 'a Types.Unit.t -> ('a Paths.Identifier.any * string) list =
   in
   process_unit unit ; !output
 
-let update_index :
-    pkg:string -> Sqlite3.db -> ('a Paths.Identifier.any * string) list -> unit =
+let update_index =
   let ok_exn =
     let open Sqlite3.Rc in
     function
@@ -138,8 +144,8 @@ let update_index :
     function
     | DONE -> () | e -> failwith (sprintf "Sqlite error: %s" (to_string e))
   in
-  let module Id = Paths.Identifier in
-  let rec name : 'a Id.any -> string = function
+  let module Id = Model.Paths.Identifier in
+  let rec name = function
     | Id.Root (_, x) | Id.Page (_, x) | Id.CoreType x | Id.CoreException x -> x
     | Id.Module (p, x)
      |Id.ModuleType (p, x)
@@ -155,18 +161,17 @@ let update_index :
     | Id.Label _ | Id.Constructor _ | Id.Argument _ | Id.Field _ ->
         failwith "No printable path."
   in
-  fun ~pkg db ids ->
-    let url id =
-      let open DocOckHtml in
-      match
-        Url.from_identifier ~get_package:(fun _ -> pkg) ~stop_before:false id
-      with
-      | Ok {page; anchor; kind} ->
-          List.rev ("index.html" :: page)
-          |> String.concat "/"
-          |> fun path -> if anchor = "" then path else path ^ "#" ^ anchor
-      | Error e -> failwith (Url.Error.to_string e)
-    in
+  let url id =
+    let open Html in
+    match Url.from_identifier ~stop_before:false id with
+    | Ok {page; anchor; _} ->
+        List.rev ("index.html" :: page)
+        |> String.concat ~sep:"/"
+        |> fun path ->
+        if String.(anchor = "") then path else path ^ "#" ^ anchor
+    | Error e -> failwith (Url.Error.to_string e)
+  in
+  fun db (ids: (Model.Paths.Identifier.any * _) list) ->
     let open Sqlite3 in
     let stmt =
       prepare db
@@ -174,7 +179,7 @@ let update_index :
     in
     exec db "BEGIN TRANSACTION;" |> ok_exn ;
     List.iter
-      (fun (id, type_) ->
+      ~f:(fun (id, type_) ->
         let open Data in
         bind stmt 1 (TEXT (name id)) |> ok_exn ;
         bind stmt 2 (TEXT type_) |> ok_exn ;
@@ -184,16 +189,12 @@ let update_index :
       ids ;
     exec db "END TRANSACTION;" |> ok_exn
 
-let add_anchors :
-    pkg:string -> Fpath.t -> ('a Paths.Identifier.any * string) list -> unit =
- fun ~pkg docu_dir ids ->
+let add_anchors docu_dir ids =
   List.fold_left
-    (fun m (id, type_) ->
-      let open DocOckHtml in
-      let url =
-        Url.from_identifier ~get_package:(fun _ -> pkg) ~stop_before:false id
-      in
-      let Url.({page; anchor}) =
+    ~f:(fun m (id, type_) ->
+      let open Html in
+      let url = Url.from_identifier ~stop_before:false id in
+      let Url.({page; anchor; _}) =
         match url with
         | Ok x -> x
         | Error e -> failwith (Url.Error.to_string e)
@@ -201,13 +202,13 @@ let add_anchors :
       let file =
         let html_path =
           List.rev ("index.html" :: page)
-          |> String.concat "/" |> Fpath.of_string |> ok_exn
+          |> String.concat ~sep:"/" |> Fpath.of_string |> ok_exn
         in
         Fpath.(docu_dir // html_path)
       in
-      if anchor = "" || not (OS.File.exists file |> ok_exn) then m
+      if String.(anchor = "") || not (OS.File.exists file |> ok_exn) then m
       else
-        let name_ = Paths.Identifier.name id in
+        let name_ = Model.Paths.Identifier.name id in
         let anchor_elem =
           Soup.create_element
             ~attributes:[("name", sprintf "//apple_ref/cpp/%s/%s" type_ name_)]
@@ -218,11 +219,11 @@ let add_anchors :
             | Some xs -> Some ((anchor, anchor_elem) :: xs)
             | None -> Some [(anchor, anchor_elem)])
           m )
-    Fpath.Map.empty ids
+    ~init:Fpath.Map.empty ids
   |> Fpath.Map.iter (fun file anchors ->
          let soup = OS.File.read file |> ok_exn |> Soup.parse in
          List.iter
-           (fun (anchor, anchor_elem) ->
+           ~f:(fun (anchor, anchor_elem) ->
              match Soup.select_one (sprintf {|a[href="#%s"]|} anchor) soup with
              | Some node -> Soup.prepend_child node anchor_elem
              | None ->
@@ -232,12 +233,12 @@ let add_anchors :
            anchors ;
          OS.File.write file (Soup.to_string soup) |> ok_exn )
 
-let main output_path pkg_names =
-  (* Create the docset template. *)
+let create_template output_path =
   let docset_dir = Fpath.of_string output_path |> ok_exn in
   let docset_name = Fpath.(rem_ext docset_dir |> basename) in
   let res_dir = Fpath.(docset_dir / "Contents" / "Resources") in
   let docu_dir = Fpath.(res_dir / "Documents") in
+  let db_file = Fpath.(res_dir / "docSet.dsidx") in
   OS.Dir.create docu_dir |> ok_exn |> ignore ;
   OS.File.write
     Fpath.(docset_dir / "Contents" / "Info.plist")
@@ -251,27 +252,10 @@ let main output_path pkg_names =
       % Fpath.(docset_dir / "icon.png" |> to_string))
     |> run_status ~quiet:true)
   |> ok_exn |> ignore ;
-  (* Generate documentation using Odoc. *)
-  Logs.info (fun m -> m "Running odoc.") ;
-  let _ =
-    OS.Cmd.(
-      Cmd.(v "odig" % "odoc" %% of_list pkg_names) |> run_status ~quiet:true)
-    |> ok_exn
-  in
-  Logs.info (fun m -> m "Done running odoc.") ;
-  let conf = Odig.Conf.of_opam_switch () |> ok_exn in
-  let doc_dir = Odig.Odoc.htmldir conf None in
-  (* Copy documentation. *)
-  Logs.info (fun m -> m "Copying documentation.") ;
-  Sys.command
-    (sprintf "cp -r %s/* %s"
-       Fpath.(to_string doc_dir)
-       Fpath.(docu_dir |> to_string))
-  |> ignore ;
-  Logs.info (fun m -> m "Done copying documentation.") ;
-  (* Create index db. *)
-  let db_file = Fpath.(res_dir / "docSet.dsidx") in
   OS.Path.delete db_file |> ok_exn ;
+  (docu_dir, db_file)
+
+let create_db db_file =
   let db = Sqlite3.db_open (Fpath.to_string db_file) in
   ignore
     (Sqlite3.exec db
@@ -280,45 +264,103 @@ let main output_path pkg_names =
   ignore
     (Sqlite3.exec db
        "CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);") ;
-  let pkgs = Odig.Pkg.set conf |> ok_exn in
-  (* Collect include directories. *)
-  let include_dirs =
-    Odig.Pkg.Set.fold
-      (fun pkg dirs -> Odig.Pkg.libdir pkg :: Odig.Pkg.cachedir pkg :: dirs)
-      pkgs []
-    |> List.map (fun d -> Odoc.Fs.Directory.of_string (Fpath.to_string d))
-  in
+  db
+
+let depends conf pkg =
+  Odig.Pkg.deps pkg |> ok_exn |> Astring.String.Set.elements
+  |> List.filter_map ~f:(fun n ->
+         match Odig.Pkg.lookup conf (Odig.Pkg.name_of_string n |> ok_exn) with
+         | Ok p -> Some p
+         | Error (`Msg e) ->
+             Logs.warn (fun m -> m "Looking up package %s failed: %s" n e) ;
+             None )
+
+let populate_db include_dirs pkgs db docu_dir =
   let builder =
     Odoc.Env.create ~important_digests:true ~directories:include_dirs
   in
-  (* Populate index. *)
+  List.iter pkgs ~f:(fun pkg ->
+      let name = Odig.Pkg.name pkg in
+      Logs.info (fun m -> m "Indexing %s." name) ;
+      insert db name "Package" (sprintf "%s/index.html" name) ;
+      let cachedir = Odig.Pkg.cachedir pkg in
+      OS.Dir.contents cachedir |> ok_exn
+      |> List.iter ~f:(fun f ->
+             Logs.debug (fun m -> m "Loading %s." (Fpath.to_string f)) ;
+             if Fpath.has_ext "odoc" f then (
+               let unit =
+                 Odoc.Compilation_unit.load
+                   (Odoc.Fs.File.of_string (Fpath.to_string f))
+               in
+               let env = Odoc.Env.build builder (`Unit unit) in
+               let expander = Odoc.Env.expander env in
+               let unit =
+                 try Xref.expand expander unit with
+                 | Not_found_s _ | Caml.Not_found -> unit
+               in
+               let ids = ids_of_unit unit in
+               update_index db ids ; add_anchors docu_dir ids ) ) )
+
+let main output_path pkg_names =
+  (* Get Odig configuration. *)
+  let conf = Odig.Conf.of_opam_switch () |> ok_exn in
+  let all_pkgs = Odig.Pkg.set conf |> ok_exn |> Odig.Pkg.Set.to_list in
+  (* Look up all the selected packages. *)
+  let pkgs =
+    match pkg_names with
+    | Some names ->
+        List.filter_map names ~f:(fun n ->
+            match Odig.Pkg.find conf n with
+            | Some pkg -> Some pkg
+            | None ->
+                Logs.err (fun m -> m "Could not find package %s." n) ;
+                None )
+    | None -> all_pkgs
+  in
+  let include_dirs =
+    List.concat_map all_pkgs ~f:(fun pkg ->
+        [Odig.Pkg.libdir pkg; Odig.Pkg.cachedir pkg] )
+    |> List.map ~f:(fun d -> Odoc.Fs.Directory.of_string (Fpath.to_string d))
+  in
+  (* Create the docset template. *)
+  let docu_dir, db_file = create_template output_path in
+  (* Generate documentation using Odoc. *)
+  Logs.info (fun m -> m "Running odoc.") ;
+  let _ =
+    let names = List.map pkgs ~f:Odig.Pkg.name in
+    OS.Cmd.(Cmd.(v "odig" % "odoc" %% of_list names) |> run_status ~quiet:true)
+    |> ok_exn
+  in
+  Logs.info (fun m -> m "Done running odoc.") ;
+  (* Copy documentation. *)
+  Logs.info (fun m -> m "Copying documentation.") ;
+  List.iter pkgs ~f:(fun pkg ->
+      Logs.debug (fun m -> m "Copying %s." (Odig.Pkg.name pkg)) ;
+      let doc_dir = Odig.Odoc.htmldir conf (Some pkg) in
+      Caml.Sys.command
+        (sprintf "cp -r %s %s"
+           Fpath.(to_string doc_dir)
+           Fpath.(docu_dir |> to_string))
+      |> ignore ) ;
+  Caml.Sys.command
+    (sprintf "cp ~/.opam/default/odoc/* %s" Fpath.(docu_dir |> to_string))
+  |> ignore ;
+  Logs.info (fun m -> m "Done copying documentation.") ;
   Logs.info (fun m -> m "Creating index.") ;
-  pkgs
-  |> Odig.Pkg.Set.iter (fun pkg ->
-         let name = Odig.Pkg.name pkg in
-         Logs.info (fun m -> m "Indexing %s." name) ;
-         insert db name "Package" (sprintf "%s/index.html" name) ;
-         let cachedir = Odig.Pkg.cachedir pkg in
-         OS.Dir.contents cachedir |> ok_exn
-         |> List.iter (fun f ->
-                if Fpath.has_ext "odoc" f then (
-                  let unit =
-                    Odoc.Unit.load (Odoc.Fs.File.of_string (Fpath.to_string f))
-                  in
-                  let env = Odoc.Env.build builder (`Unit unit) in
-                  let expander = Odoc.Env.expander env in
-                  let unit =
-                    try DocOck.expand expander unit with _ -> unit
-                  in
-                  let ids = ids_of_unit unit in
-                  update_index ~pkg:name db ids ;
-                  add_anchors ~pkg:name docu_dir ids ) ) ) ;
+  let db = create_db db_file in
+  populate_db include_dirs pkgs db docu_dir ;
   Logs.info (fun m -> m "Done creating index.")
 
 let () =
   Logs.set_reporter (Logs.format_reporter ()) ;
+  Logs.set_level (Some Logs.Debug) ;
   if Array.length Sys.argv < 2 then
-    print_endline "Usage: odoc2docset DOCSET PKG..."
+    Stdio.print_endline "Usage: odoc2docset DOCSET PKG..."
   else
-    main Sys.argv.(1)
-      (Array.sub Sys.argv 2 (Array.length Sys.argv - 2) |> Array.to_list)
+    let docset_dir = Sys.argv.(1) in
+    let pkgs =
+      Array.sub Sys.argv ~pos:2 ~len:(Array.length Sys.argv - 2)
+      |> Array.to_list
+    in
+    let pkgs = if List.is_empty pkgs then None else Some pkgs in
+    main docset_dir pkgs
